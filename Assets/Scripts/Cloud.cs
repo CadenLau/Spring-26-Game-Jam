20d369e840b8e1b440aa060713aa27a650d25e28
Caden Lau <ctl65@cornell.edu>
2026-02-22 12:28:27 -0500

    Update balancing

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using DigitalRuby.LightningBolt;
 using UnityEngine;
 
@@ -31,7 +33,7 @@ public class Cloud : MonoBehaviour
         BoxCollider2D cloudCollider = GetComponent<BoxCollider2D>();
         minX = transform.position.x - cloudCollider.bounds.size.x / 2f; 
         maxX = transform.position.x + cloudCollider.bounds.size.x / 2f; 
-        nextRainSpawnTime = Random.Range(rainSpawnRate - (rainSpawnRateRange / 2), 
+        nextRainSpawnTime = UnityEngine.Random.Range(rainSpawnRate - (rainSpawnRateRange / 2), 
             rainSpawnRate + (rainSpawnRateRange / 2));
     }
 
@@ -39,30 +41,41 @@ public class Cloud : MonoBehaviour
     {
         if (Time.time >= nextRainSpawnTime) 
         {
-            nextRainSpawnTime = Time.time + Random.Range(rainSpawnRate - (rainSpawnRateRange / 2), 
+            nextRainSpawnTime = Time.time + UnityEngine.Random.Range(rainSpawnRate - (rainSpawnRateRange / 2), 
                 rainSpawnRate + (rainSpawnRateRange / 2));
             SpawnRaindrop();
         }
         if (Time.time >= nextLightningSpawnTime) 
         {
-            nextLightningSpawnTime = Time.time + Random.Range(lightningSpawnRate - 
+            nextLightningSpawnTime = Time.time + UnityEngine.Random.Range(lightningSpawnRate - 
             (lightningSpawnRateRange / 2), 
                 lightningSpawnRate + (lightningSpawnRateRange / 2));
             SpawnLightning();
         }
     }
 
+    private List<float> sections = new() {0f, 1f};
+    private static readonly List<float> allSections = new() {0f, 1f};
     private void SpawnRaindrop()
     {
+        float width = Math.Abs(maxX - minX);
+        if (sections.Count == 0) 
+        {
+            sections = new(allSections);
+        }
+        int getSection = UnityEngine.Random.Range(0, sections.Count);
+        float section = sections[getSection]; // randomly select a section and remove it from the list
+        sections.RemoveAt(getSection);
+        
         // Instantiate the object at the spawner's position and rotation
-        Instantiate(raindrop, new Vector2(Random.Range(minX, maxX), transform.position.y), 
+        Instantiate(raindrop, new Vector2(UnityEngine.Random.Range(minX + width * (section / allSections.Count), minX + width * ((section + 1) / allSections.Count)), transform.position.y), 
             transform.rotation);
     }
 
     private async void SpawnLightning()
     {   
         GameObject indicator = Instantiate(warningBox, Vector2.zero, transform.rotation);
-        float minLightningX = Random.Range(minX, maxX);
+        float minLightningX = UnityEngine.Random.Range(minX, maxX);
         float maxLightningX = minLightningX;
 
         DrawBox indicatorScript = indicator.GetComponent<DrawBox>();
